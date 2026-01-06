844140847f61cd2c302eb33212cfacda236ebdce
RealHarlex <xxxharliy@gmail.com>
2026-01-06 13:20:16 +0100

    C00

---
+++ Days/C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write (1, &c, 1);
+}
+
+void    ft_print_reverse_alphabet(void)
+{
+    char c = 'z';
+
+    while(c >= 'a')
+    {
+        ft_putchar(c);
+        c--;
+    }
+}
+
+// int main()
+// {
+//     ft_print_reverse_alphabet();
+//     return 0;
+// }
